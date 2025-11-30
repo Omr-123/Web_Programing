479027af796569c4c28c30e05537f7e95dd76712
Guest <guest@example.com>
2025-11-30 19:37:54 +0200

    Bugs fix

--- assets/js/courses.js
+++ assets/js/courses.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     $('.course').each(function (index) {
         $(this).delay(150 * index).animate({
             opacity: 1,
-        }, 400);
+        }, 100);
     });
 });
