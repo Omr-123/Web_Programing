095ad378ff7bf6df23fa14ad924023b3ad263dea
Guest <guest@example.com>
2025-12-14 14:32:41 +0200

    Fixes

--- register.php
+++ register.php
@@ -55,22 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 </head>
 
 <body>
-
-    <div class="header">
-        <div class="nav">
-            <a class="nav-logo" href="index.php">
-                <img src="assets/images/Lerno.png" alt="Logo">
-            </a>
-            <ul class="nav-links">
-                <li class="nav-link"><a href="index.php">Home</a></li>
-                <li class="nav-link"><a href="courses.php">Courses</a></li>
-                <li class="nav-link"><a href="my-courses.php">My Courses</a></li>
-                <li class="nav-link"><a href="login.php">Login</a></li>
-                <li class="nav-link active"><a href="register.html">Register</a></li>
-                <li class="nav-link"><a href="cart.php">Cart</a></li>
-            </ul>
-        </div>
-    </div>
+    <?php include("components/navbar.php") ?>
 
     <div class="auth-wrapper">
         <div class="auth-container">
